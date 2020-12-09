441dafe3eb309efa0ac456bb95db12868d77973c
DYLAN HANNIGAN <dh8531@students.necsd.net>
2020-12-09 01:54:53 +0000

    did lab problems 123

--- Prob1.java
+++ Prob1.java
@@ -1,9 +1,20 @@
 import java.util.Scanner;
 
 public class Prob1 {
-  public static void main(String[] args){
+public static void main(String[] args)
+{
+//Declare a scanner and input an int.
+Scanner scan = new Scanner(System.in);
+System.out.println("Please enter an integer:");
+int n = scan.nextInt();
+//If n is not in the range, print True.
+if ( !( n >= 5 && n <= 76))
+System.out.println("True");
+//Otherwise, print False.
+else
+System.out.println("False");
+}
+}
 
 
 
-  }
-  }
